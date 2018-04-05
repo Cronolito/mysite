4435eb0d68ba66d1ee542139fdb499215bee27c4
Cronolito <da87ka@gmail.com>
2018-04-05 22:19:05 +0200

    Startat pa 3

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,7 @@ from . import views
 #Path har argumenten URLPath och sen viewfunktionen som man kan namege med name= for att kunna hitta senare.
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
